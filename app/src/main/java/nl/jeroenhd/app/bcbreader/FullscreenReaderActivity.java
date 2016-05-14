df7ae6712822d25ac8f519d3d591e41a4cd80d19
Jeroen Hoegen Dijkhof <git@jeroenhd.nl>
2016-05-14 18:47:15 +0200

    Improvements to full screen view

--- app/src/main/java/nl/jeroenhd/app/bcbreader/FullscreenReaderActivity.java
+++ app/src/main/java/nl/jeroenhd/app/bcbreader/FullscreenReaderActivity.java
@@ -40,6 +40,20 @@ public class FullscreenReaderActivity extends AppCompatActivity implements View.
     private static final int UI_ANIMATION_DELAY = 300;
     private final FullscreenReaderActivity thisActivity = this;
     private final Handler mHideHandler = new Handler();
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
     Button buttonPrev, buttonNext;
     SeekBar seekBar;
     private View mContentView;
@@ -77,20 +91,6 @@ public class FullscreenReaderActivity extends AppCompatActivity implements View.
         @Override
         public void run() {
             hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
         }
     };
     private Chapter currentChapter;
@@ -275,6 +275,7 @@ public class FullscreenReaderActivity extends AppCompatActivity implements View.
         {
             viewPager.setCurrentItem(progress);
         }
+        setTitle(String.format("%s: page %d", currentChapter.getTitle(), progress + 1));
     }
 
     @Override
