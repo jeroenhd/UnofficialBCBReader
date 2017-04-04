892ad5696ee51de10de86c4fd1e3b162ccb0fb15
Jeroen Hoegen Dijkhof <git@jeroenhd.nl>
2017-04-04 11:06:03 +0200

    Add basic crash reporter

---
+++ app/src/main/java/nl/jeroenhd/app/bcbreader/tools/AppCrashStorage.java
@@ -0,0 +1,70 @@
+package nl.jeroenhd.app.bcbreader.tools;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+import nl.jeroenhd.app.bcbreader.BuildConfig;
+import nl.jeroenhd.app.bcbreader.data.App;
+import nl.jeroenhd.app.bcbreader.data.Telemetry;
+
+/**
+ * A class to store and retrieve crash data
+ */
+public class AppCrashStorage {
+    /**
+     * The app data directory to store all crash logs in
+     */
+    private static final String CRASH_DIRECTORY = "sunken_ships";
+    private static final String CRASH_LOG_HEADER = "This is a crash log for the unofficial BCB Reader\nNo personal information is present in this file!\n";
+    /**
+     * The context used to get file access
+     */
+    private Context context;
+
+    /**
+     * Create a new AppCrashStorage object
+     *
+     * @param context The context used to get file access
+     */
+    public AppCrashStorage(Context context) {
+        this.context = context;
+    }
+
+    /**
+     * Store a crash log in a file so it can be sent next time
+     *
+     * @param crashedThread  The crashed thread
+     * @param crashThrowable The exception that was not caught
+     */
+    public void StoreCrash(Thread crashedThread, Throwable crashThrowable) {
+        // If this crashes as well... then all hope is lost!
+        // Just put a try/catch handler around this code that doesn't do much
+        try {
+            File crashDirectory = context.getDir(CRASH_DIRECTORY, 0);
+            String fileName = System.currentTimeMillis() + "-" + App.Version() + "-" + crashedThread.getName() + ".crash";
+            File outputFile = new File(crashDirectory, fileName);
+
+            Telemetry deviceInformation = Telemetry.getInstance(context);
+
+            PrintWriter printWriter = new PrintWriter(outputFile);
+            printWriter.write(CRASH_LOG_HEADER);
+            printWriter.write("App version number: " + BuildConfig.VERSION_CODE + "(" + BuildConfig.VERSION_NAME + ")\n");
+            printWriter.write("Basic device information:\nModel: " + deviceInformation.getModel()
+                    + "\nAndroid version: " + deviceInformation.getAndroidVersion() + "\n======\n");
+            printWriter.write("Crashed thread: " + crashedThread.getName() + "\n");
+            printWriter.write("Stack trace:\n");
+            crashThrowable.printStackTrace(printWriter);
+            printWriter.flush();
+            printWriter.close();
+        } catch (Exception e) {
+            Log.e(App.TAG, "An exception occurred processing an uncaught error! Some information might not have been written to disk!");
+            Log.e(App.TAG, "This happened:");
+            e.printStackTrace();
+            Log.e(App.TAG, "While processing this exception:");
+            crashThrowable.printStackTrace();
+        }
+    }
+}
